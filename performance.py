97e24c05f2e1e0d660141bbecaf88b9ccd7e689a
Antonio Galanti <antonio.galanti@gmail.com>
2026-01-17 15:19:21 +0100

    added more gentle KeyboardInterrupt handling

--- performance.py
+++ performance.py
@@ -33,8 +33,6 @@ from performance_utils import drop_disk_cache, create_path_batch, Precision
 
 cuda_device = 0
 dateset_path = Path("/home/antonio/Downloads/extended_smartdoc_dataset/Extended Smartdoc dataset/train")
-batch_sizes = [1, 10, 20, 50, 100, 150, 200, 250, 300, 350, 400, 700, 1000, 1200, 1700, 2500, 3000, 3500, 4000, 5000, 10000, 15000, 22000]
-batches = list(map(lambda size: create_path_batch(dateset_path, size), batch_sizes)) # eager
 
 
 def batch_to_gpu(paths: List[str | np.ndarray], max_h: int, max_w: int, progress_bar):
@@ -95,76 +93,103 @@ def compute_gpu(batch: List[str], vram_commitment):
 
 
 def compute_cpu(path: str, verbose=False):
-    start_imload = time.time()
-    image = cv2.imread(
-        filename=path,
-        flags=cv2.IMREAD_GRAYSCALE,
-    )  # RETURNS (H, W), NOT (W, H)!
-    time_imload = time.time() - start_imload
-    if verbose:
-        print(f"Loading an image from disk took {time_imload}")
+    try:
+        start_imload = time.time()
+        image = cv2.imread(
+            filename=path,
+            flags=cv2.IMREAD_GRAYSCALE,
+        )  # RETURNS (H, W), NOT (W, H)!
+        time_imload = time.time() - start_imload
+        if verbose:
+            print(f"Loading an image from disk took {time_imload}")
 
-    image = np.divide(image, 255.0)
+        image = np.divide(image, 255.0)
 
-    if verbose:
-        print(f"The image stays on CPU")
+        if verbose:
+            print(f"The image stays on CPU")
 
-    start_compute_time = time.time()
-    whites = coords_from_segmentation_mask(image, Device.CPU)
-    compute_time = time.time() - start_compute_time
-    if verbose:
-        print(f"Coords extraction took {compute_time}")
+        start_compute_time = time.time()
+        whites = coords_from_segmentation_mask(image, Device.CPU)
+        compute_time = time.time() - start_compute_time
+        if verbose:
+            print(f"Coords extraction took {compute_time}")
 
-    return time.time() - start_imload
+        return time.time() - start_imload
+    except KeyboardInterrupt:
+        return None
 
 
 
-def compute_cpu_multiprocess(paths, cores=cpu_count() - 1, verbose=False):
+def _multiprocess_wrapper(args):
+    return compute_cpu(*args)
+
+def compute_cpu_multiprocess(paths, cores, progress_bar=None, verbose=False):
+    if not cores:
+        raise ValueError("`cores` is required for `compute_cpu_multiprocess`")
+
     start_t = time.time()
     paths = [str(p) for p in paths]
     args = map(lambda p: (p, verbose), paths)
+
     with Pool(processes=cores) as pool:
-        pool.starmap(
-            compute_cpu,
-            args
-        )
+        for _ in pool.imap_unordered(_multiprocess_wrapper, args):
+            if progress_bar:
+                progress_bar.update(1)
     return time.time() - start_t
 
 
-def benchmark_cpu():
+def benchmark_cpu(
+        batch_sizes: List[int],
+        csv_file: str | None = None,
+        n_cores=cpu_count(),
+        verbose=False
+):
+    print("Starting CPU benchmark with parameters:")
+    print(locals())
     # password = getpass.getpass("Enter your password to invalidate disk cache: ") # if stdin is not accessible it throws an exception
-    csv_file = "compute_durations_cpu_multiprocess.csv"
-    try:
-        durations = pd.read_csv(csv_file, index_col=False)
-    except FileNotFoundError:
+    if csv_file:
+        try:
+            durations = pd.read_csv(csv_file, index_col=False)
+        except FileNotFoundError:
+            durations = pd.DataFrame([], columns=["batch_size", "duration"], index=None)
+    else:
         durations = pd.DataFrame([], columns=["batch_size", "duration"], index=None)
+
 
     for i, b in enumerate(batches):
         try:
             size = batch_sizes[i]
             if size in durations["batch_size"].values:
-                print(f"Batch {i + 1} ({size} items) already computed. Skipping...")
+                if verbose:
+                    print(f"Batch {i + 1} ({size} items) already computed. Skipping...")
                 continue
 
-            print(f"Computing batch {i + 1}, {size} images.")
-            t = compute_cpu_multiprocess(b, 8)
-            print(f"Took {t} seconds.")
+            if verbose:
+                print(f"Computing batch {i + 1}, {size} images.")
+            with tqdm(total=size, leave=False, desc=f"Batch {i + 1}: ") as progress_bar:
+                t = compute_cpu_multiprocess(b, n_cores, progress_bar)
+            if verbose:
+                print(f"Took {t} seconds.")
             # drop_disk_cache(password, verbose=True)
             durations = pd.concat(
                 objs=[durations, pd.DataFrame({'batch_size': [size], 'duration': [t]})],
                 ignore_index=True
             )
-            print(f"Batch {i + 1} done.")
+            if verbose:
+                print(f"Batch {i + 1} done.")
         except KeyboardInterrupt:
             break
-
-    durations.sort_values(by=["batch_size"], inplace=True)
-    durations.to_csv(
-        csv_file,
-        mode="w",
-        header=True,
-        index=False
-    )
+    # This is python :D
+    if csv_file:
+        durations.sort_values(by=["batch_size"], inplace=True)
+        durations.to_csv(
+            csv_file,
+            mode="w",
+            header=True,
+            index=False
+        )
+        return None
+    else: return durations
 
 
 
@@ -202,18 +227,44 @@ def benchmark_gpu(vram_commitment=10*(1024**3)):
             print(f"Batch {i + 1} done.")
         except KeyboardInterrupt:
             break
-    durations.sort_values(by=["batch_size"], inplace=True)
-    durations.to_csv(
-        csv_file,
-        mode="w",
-        header=True,
-        index=False
-    )
+    try:
+        durations.sort_values(by=["batch_size"], inplace=True)
+        durations.to_csv(
+            csv_file,
+            mode="w",
+            header=True,
+            index=False
+        )
+    except KeyboardInterrupt:
+        durations.sort_values(by=["batch_size"], inplace=True)
+        durations.to_csv(
+            csv_file,
+            mode="w",
+            header=True,
+            index=False
+        )
+
 
 
 if __name__ == "__main__":
-    benchmark_gpu(vram_commitment=14 * (1024**3))
-    # benchmark_cpu()
+    batch_sizes = [1, 10, 20, 50, 100, 150, 200, 250, 300, 350, 400, 700, 1000, 1200, 1700, 2500, 3000, 3500, 4000, 5000, 10000, 15000, 22000]
+    batches = list(map(lambda size: create_path_batch(dateset_path, size), batch_sizes)) # eager
+    all_paths = [
+        path
+        for path_group in batches
+        for path in path_group
+    ]    # total_paths = [element for sublist in nested_list for element in sublist]
+    paths_len = len(batches)
+    print("Starting Benchmark.")
+
+    val = benchmark_cpu(
+        # csv_file="./compute_durations_cpu_multiprocess.csv",
+        csv_file='./test.csv',
+        batch_sizes=batch_sizes,
+        n_cores=cpu_count() -  1
+    )
+    print(val)
+    # benchmark_gpu(vram_commitment=14 * (1024**3))
 
 
 
