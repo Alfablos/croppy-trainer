adding tunable percision; implemented hybrid performance benchmark, implemented plotting mechanism, added methods for Precision to return the correct type

performance.py
@@ -8,8 +8,12 @@ The function that implements the algorithm
 4. TODO: All of the above with lower precision (FP16, UINT8)
 5. TODO: Federated with a cloud instance
 """
-
+import logging
+from logging import info, debug
+from datetime import datetime
 import os
+import math
+import threading
 from asyncio import CancelledError
 from sys import stderr
 import subprocess
@@ -24,7 +28,12 @@ import pandas as pd
 import cv2
 import numpy as np
 import torch
-from pynvml import nvmlInit, nvmlDeviceGetHandleByIndex, nvmlDeviceGetMemoryInfo, c_nvmlVgpuTypeIdInfo_v1_t
+from pynvml import (
+    nvmlInit,
+    nvmlDeviceGetHandleByIndex,
+    nvmlDeviceGetMemoryInfo,
+    c_nvmlVgpuTypeIdInfo_v1_t,
+)
 from tqdm import tqdm
 
 from common import Device
@@ -32,7 +41,9 @@ from utils import coords_from_segmentation_mask, find_max_dims
 from performance_utils import drop_disk_cache, create_path_batch, Precision
 
 cuda_device = 0
-dateset_path = Path("/home/antonio/Downloads/extended_smartdoc_dataset/Extended Smartdoc dataset/train")
+dateset_path = Path(
+    "/home/antonio/Downloads/extended_smartdoc_dataset/Extended Smartdoc dataset/train"
+)
 
 
 def batch_to_gpu(paths: List[str | np.ndarray], max_h: int, max_w: int, progress_bar):
@@ -57,7 +68,9 @@ def batch_to_gpu(paths: List[str | np.ndarray], max_h: int, max_w: int, progress
     return tensor.to(Device.CUDA.value)
 
 
-def images_for_vram(vram_commitment, max_h, max_w, precision: Precision = Precision.FP32):
+def images_for_vram(
+    vram_commitment, max_h, max_w, precision: Precision = Precision.FP32
+):
     """
     Computes the number of images that can be stored in the given amount of memory.
     The resuslt is rounded by excess in case all the images have max_h and max_w
@@ -69,9 +82,8 @@ def images_for_vram(vram_commitment, max_h, max_w, precision: Precision = Precis
     """
     # n x h x w x P (If P = float32 => 4 bytes) = ram_commitment
     # n = ram_commitment / (h * w * 4)
-    img_size = (max_h * max_w * precision.value)
-    return vram_commitment // img_size # integer division floors the value
-
+    img_size = max_h * max_w * precision.value
+    return int(vram_commitment // img_size)  # integer division floors the value
 
 
 def compute_gpu(batch: List[str], vram_commitment):
@@ -80,10 +92,22 @@ def compute_gpu(batch: List[str], vram_commitment):
     total_images = len(batch)
     start = time.time()
 
-    with tqdm(total=total_images, leave=True, position=0, desc=f"Uploading {total_images} images to GPU") as uploadbar:
-        with tqdm(total=total_images, leave=True, position=1, desc="Computing mask corner coordinates") as workbar:
+    with tqdm(
+        total=total_images,
+        leave=True,
+        position=0,
+        desc=f"Uploading {total_images} images to GPU",
+    ) as uploadbar:
+        with tqdm(
+            total=total_images,
+            leave=True,
+            position=1,
+            desc="Computing mask corner coordinates",
+        ) as workbar:
             for i in range(0, total_images, max_images):
-                tensor = batch_to_gpu(batch[i : i + max_images], max_h, max_w, uploadbar)
+                tensor = batch_to_gpu(
+                    batch[i : i + max_images], max_h, max_w, uploadbar
+                )
                 minibatch_size = tensor.shape[0]
                 for img in range(minibatch_size):
                     _ = coords_from_segmentation_mask(tensor[img], device=Device.CUDA)
@@ -101,27 +125,27 @@ def compute_cpu(path: str, verbose=False):
         )  # RETURNS (H, W), NOT (W, H)!
         time_imload = time.time() - start_imload
         if verbose:
-            print(f"Loading an image from disk took {time_imload}")
+            info(f"Loading an image from disk took {time_imload}")
 
         image = np.divide(image, 255.0)
 
         if verbose:
-            print(f"The image stays on CPU")
+            info(f"The image stays on CPU")
 
         start_compute_time = time.time()
         whites = coords_from_segmentation_mask(image, Device.CPU)
         compute_time = time.time() - start_compute_time
         if verbose:
-            print(f"Coords extraction took {compute_time}")
+            info(f"Coords extraction took {compute_time}")
 
         return time.time() - start_imload
     except KeyboardInterrupt:
         return None
 
 
-
 def _multiprocess_wrapper(args):
     return compute_cpu(*args)
+
 
 def compute_cpu_multiprocess(paths, cores, progress_bar=None, verbose=False):
     if not cores:
@@ -139,13 +163,16 @@ def compute_cpu_multiprocess(paths, cores, progress_bar=None, verbose=False):
 
 
 def benchmark_cpu(
-        batch_sizes: List[int],
-        csv_file: str | None = None,
-        n_cores=cpu_count(),
-        verbose=False
+    batches: List[List[str]],
+    batch_sizes: List[int],
+    csv_file: str | None = None,
+    n_cores=cpu_count(),
+    verbose=False,
 ):
-    print("Starting CPU benchmark with parameters:")
-    print(locals())
+    f_args = locals()
+    f_args["batches"] = "<Redacted: too long>"
+    debug("Starting CPU benchmark with parameters:")
+    debug(f_args)
     # password = getpass.getpass("Enter your password to invalidate disk cache: ") # if stdin is not accessible it throws an exception
     if csv_file:
         try:
@@ -155,51 +182,68 @@ def benchmark_cpu(
     else:
         durations = pd.DataFrame([], columns=["batch_size", "duration"], index=None)
 
-
-    for i, b in enumerate(batches):
-        try:
-            size = batch_sizes[i]
-            if size in durations["batch_size"].values:
-                if verbose:
-                    print(f"Batch {i + 1} ({size} items) already computed. Skipping...")
-                continue
-
-            if verbose:
-                print(f"Computing batch {i + 1}, {size} images.")
-            with tqdm(total=size, leave=False, desc=f"Batch {i + 1}: ") as progress_bar:
-                t = compute_cpu_multiprocess(b, n_cores, progress_bar)
-            if verbose:
-                print(f"Took {t} seconds.")
-            # drop_disk_cache(password, verbose=True)
-            durations = pd.concat(
-                objs=[durations, pd.DataFrame({'batch_size': [size], 'duration': [t]})],
-                ignore_index=True
-            )
-            if verbose:
-                print(f"Batch {i + 1} done.")
-        except KeyboardInterrupt:
-            break
-    # This is python :D
-    if csv_file:
-        durations.sort_values(by=["batch_size"], inplace=True)
-        durations.to_csv(
-            csv_file,
-            mode="w",
-            header=True,
-            index=False
-        )
-        return None
-    else: return durations
-
-
-
-def benchmark_gpu(vram_commitment=10*(1024**3)):
-    print(f"Starting GPU benchmark with {vram_commitment/(1024**3)} GB of commited VRAM")
-    csv_file = "compute_durations_gpu_iter.csv"
     try:
-        durations = pd.read_csv(csv_file, index_col=False)
-    except FileNotFoundError:
-        durations = pd.DataFrame([], columns=["batch_size", "duration", "vram_usage"], index=None)
+        for i, b in enumerate(batches):
+            try:
+                size = batch_sizes[i]
+                if size in durations["batch_size"].values:
+                    if verbose:
+                        debug(
+                            f"Batch {i + 1} ({size} items) already computed. Skipping..."
+                        )
+                    continue
+
+                if verbose:
+                    info(f"Computing batch {i + 1}, {size} images.")
+                with tqdm(
+                    total=size, leave=False, desc=f"Batch {i + 1}: "
+                ) as progress_bar:
+                    t = compute_cpu_multiprocess(b, n_cores, progress_bar)
+                if verbose:
+                    debug(f"Took {t} seconds.")
+                # drop_disk_cache(password, verbose=True)
+                durations = pd.concat(
+                    objs=[
+                        durations,
+                        pd.DataFrame({"batch_size": [size], "duration": [t]}),
+                    ],
+                    ignore_index=True,
+                )
+                if verbose:
+                    debug(f"Batch {i + 1} done.")
+            except KeyboardInterrupt:
+                break
+    finally:
+        # This is python :D
+        if csv_file:
+            durations.sort_values(by=["batch_size"], inplace=True)
+            durations.to_csv(csv_file, mode="w", header=True, index=False)
+            return None
+        else:
+            return durations
+
+
+def benchmark_gpu(
+    vram_commitment,
+    batches: List[List[str]],
+    batch_sizes: List[int],
+    csv_file: str | None = None,
+):
+    f_args = locals()
+    f_args["batches"] = "<Redacted: too long>"
+    debug("Starting GPU benchmark with parameters:")
+    debug(f_args)
+    if csv_file:
+        try:
+            durations = pd.read_csv(csv_file, index_col=False)
+        except FileNotFoundError:
+            durations = pd.DataFrame(
+                [], columns=["batch_size", "duration", "vram_usage"], index=None
+            )
+    else:
+        durations = pd.DataFrame(
+            [], columns=["batch_size", "duration", "vram_usage"], index=None
+        )
 
     # nvmlInit()
     # cuda_d = nvmlDeviceGetHandleByIndex(cuda_device)
@@ -208,63 +252,200 @@ def benchmark_gpu(vram_commitment=10*(1024**3)):
             try:
                 size = batch_sizes[i]
                 if size in durations["batch_size"].values:
-                    print(f"Batch {i + 1} ({size} items) already computed. Skipping...")
+                    debug(f"Batch {i + 1} ({size} items) already computed. Skipping...")
                     continue
 
-                print(f"Computing batch {i + 1}, {size} images.")
+                info(f"Computing batch {i + 1}, {size} images.")
                 t = compute_gpu(b, vram_commitment)
-                print(f"Took {t} seconds.")
+                debug(f"Took {t} seconds.")
                 # drop_disk_cache(password, verbose=True)
                 # cuda_info = nvmlDeviceGetMemoryInfo(cuda_d)
                 durations = pd.concat(
-                    objs=[durations, pd.DataFrame({
-                        'batch_size': [size],
-                        'duration': [t],
-                        # cannot use vram_usage due to del tensor in compute_gpu()
-                        # 'vram_usage': cuda_info.used
-                    })],
-                    ignore_index=True
+                    objs=[
+                        durations,
+                        pd.DataFrame(
+                            {
+                                "batch_size": [size],
+                                "duration": [t],
+                                # cannot use vram_usage due to del tensor in compute_gpu()
+                                # 'vram_usage': cuda_info.used
+                            }
+                        ),
+                    ],
+                    ignore_index=True,
                 )
-                print(f"Batch {i + 1} done.")
+                debug(f"Batch {i + 1} done.")
             except KeyboardInterrupt:
                 break
     finally:
-        durations.sort_values(by=["batch_size"], inplace=True)
-        durations.to_csv(
-            csv_file,
-            mode="w",
-            header=True,
-            index=False
+        if csv_file:
+            durations.sort_values(by=["batch_size"], inplace=True)
+            durations.to_csv(csv_file, mode="w", header=True, index=False)
+        else:
+            return durations
+
+
+def compute_hybrid(
+    cpu_split: float,
+    n_cores,
+    vram_commitment,
+    batches: List[List[str]],
+    batch_sizes: List[int],
+    csv_file: str | None = None,
+):
+    f_args = locals()
+    f_args["batches"] = "<Redacted: too long>"
+    debug("Starting HYBRID benchmark with parameters:")
+    debug(f_args)
+
+    if csv_file:
+        try:
+            durations = pd.read_csv(csv_file, index_col=False)
+        except FileNotFoundError:
+            durations = pd.DataFrame(
+                [], columns=["batch_size", "duration", "vram_usage"], index=None
+            )
+    else:
+        durations = pd.DataFrame(
+            [], columns=["batch_size", "duration", "vram_usage"], index=None
         )
 
+    try:
+        for i, b in enumerate(batches):
+            size = len(b)
+            if size == 1:
+                raise ValueError("Cannot split batches of length 1, they'd only be computed by the GPU.")
+            try:
+                split_idx = int(size * cpu_split) # floor
+                cpu_batch = b[:split_idx]
+                gpu_batch = b[split_idx:]
 
-def compute_hybrid():
-    pass
+                if size in durations["batch_size"].values:
+                    debug(f"Batch {i + 1} ({size} items) already computed. Skipping...")
+                    continue
+
+                info(f"Computing batch {i + 1}, {size} images.")
+
+                start_time = time.time()
+                if cpu_batch:
+                    def _run_cpu(cores, batch, f):
+                        return f(batch, cores)
+                    cpu_thread = threading.Thread(target=_run_cpu, args=(n_cores, cpu_batch, compute_cpu_multiprocess))
+                    cpu_thread.start()
+                if gpu_batch:
+                    _ = compute_gpu(gpu_batch, vram_commitment)
+
+                t = time.time() - start_time
+                debug(f"Took {t} seconds.")
+                # drop_disk_cache(password, verbose=True)
+                # cuda_info = nvmlDeviceGetMemoryInfo(cuda_d)
+                durations = pd.concat(
+                    objs=[
+                        durations,
+                        pd.DataFrame(
+                            {
+                                "batch_size": [size],
+                                "duration": [t],
+                                # cannot use vram_usage due to del tensor in compute_gpu()
+                                # 'vram_usage': cuda_info.used
+                            }
+                        ),
+                    ],
+                    ignore_index=True,
+                )
+                print(f"Batch {i + 1} done.")
+                if csv_file:
+                    durations.sort_values(by=["batch_size"], inplace=True)
+                    durations.to_csv(csv_file, mode="w", header=True, index=False)
+
+            except KeyboardInterrupt:
+                break
+    finally:
+        if csv_file:
+            durations.sort_values(by=["batch_size"], inplace=True)
+            durations.to_csv(csv_file, mode="w", header=True, index=False)
+        else:
+            return durations
+
+class TqdmHandler(logging.Handler):
+    def emit(self, record):
+        tqdm.write(self.format(record))
+
 
 
 if __name__ == "__main__":
-    batch_sizes = [1, 10, 20, 50, 100, 150, 200, 250, 300, 350, 400, 700, 1000, 1200, 1700, 2500, 3000, 3500, 4000, 5000, 10000, 15000, 22000]
-    batches = list(map(lambda size: create_path_batch(dateset_path, size), batch_sizes)) # eager
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    file_handler = logging.FileHandler("performance.log", mode='a')
+    file_handler.setFormatter(logging.Formatter('%(asctime)s - %(message)s', datefmt='%H:%M:%S'))
+    logger.addHandler(file_handler)
+    logger.addHandler(TqdmHandler())
+
+    logger.info("")
+    logger.info(f"{datetime.now()}")
+
+    batch_sizes = [
+        # 1,
+        10,
+        20,
+        50,
+        100,
+        150,
+        200,
+        250,
+        300,
+        350,
+        400,
+        700,
+        1000,
+        1200,
+        1700,
+        2500,
+        3000,
+        3500,
+        4000,
+        5000,
+        10000,
+        15000,
+        # 22000,
+    ]
+    batches = list(
+        map(lambda size: create_path_batch(dateset_path, size), batch_sizes)
+    )  # eager
     all_paths = [
-        path
-        for path_group in batches
-        for path in path_group
-    ]    # total_paths = [element for sublist in nested_list for element in sublist]
+        path for path_group in batches for path in path_group
+    ]  # total_paths = [element for sublist in nested_list for element in sublist]
     paths_len = len(batches)
-    print("Starting Benchmark.")
+    debug("Starting Benchmark.")
 
-    val = benchmark_cpu(
-        # csv_file="./compute_durations_cpu_multiprocess.csv",
-        csv_file='./test.csv',
+    # val = benchmark_cpu(
+    #     # csv_file="./compute_durations_cpu_multiprocess.csv",
+    #     csv_file='./test.csv',
+    #     batches=batches,
+    #     batch_sizes=batch_sizes,
+    #     n_cores=cpu_count() -  1
+    # )
+    # val = benchmark_gpu(
+    #     batches=batches,
+    #     batch_sizes=batch_sizes,
+    #     # csv_file = "compute_durations_gpu_iter.csv",
+    #     vram_commitment=14 * (1024**3)
+    # )
+
+    n_cores = cpu_count() - 5
+    cpu_split = 0.8
+    vram_commitment = 14.5 * (1024**3)
+    precision = Precision.FP32
+    csv_file = f"./compute_durations_hybrid_{n_cores}c_{cpu_split}cpu_{vram_commitment/ (1024**3)}GVRAM_{precision}.csv"
+
+    val = compute_hybrid(
+        cpu_split=cpu_split,
+        n_cores=n_cores,
+        vram_commitment=vram_commitment,
+        csv_file=csv_file,
+        batches=batches,
         batch_sizes=batch_sizes,
-        n_cores=cpu_count() -  1
+        # precision=precision
     )
+
     print(val)
-    # benchmark_gpu(vram_commitment=14 * (1024**3))
-
-
-
-
-
-
-