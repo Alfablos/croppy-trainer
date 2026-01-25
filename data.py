399cd75405134f34f7844d073cc79c61f7d4609c
Antonio Galanti <antonio.galanti@gmail.com>
2026-01-25 03:15:29 +0100

    reimplemented __getitem__ for dataset

--- data.py
+++ data.py
@@ -1,3 +1,5 @@
+from PIL.Image import new
+import torchvision.tv_tensors
 from numpy.typing import NDArray
 from cffi.cparser import lock
 from architecture import Architecture
@@ -68,34 +70,42 @@ class SmartDocDataset(Dataset):
 
         env = self._get_or_init_env()
         with env.begin(write=False) as transaction:
-            # pickle.loads RETURNS A TUPLE FOR THE IMAGE!
-            image: NDArray = pickle.loads(transaction.get(img_idx.encode("ascii")))
+            image: NDArray = pickle.loads(transaction.get(img_idx.encode("ascii")))# shape = (h, w, 3)
             label: NDArray = pickle.loads(transaction.get(lbl_idx.encode("ascii")))
 
-            if isinstance(image, tuple):  # TODO: investigate
-                image = image[0]
-            else:
-                image = image
-
-            if isinstance(label, tuple):
-                label = label[0]
-            else:
-                label = label
-
+        h, w = image.shape[:2]
+        image_tvtensor = transformsV2.ToImage()(image) # shape is now (3, h, w)
+        
+        # For labels we need shape (4, 2): [[x1, y1], [x2, y2], ...]
+        label_reshaped = label.reshape(-1, 2)
+        
+        # Original coordinates: TODO NO normalization in coords_from_mask
+        original_coords = label_reshaped * torch.tensor([w, h], dtype=torch.float32) # [w, h] and not [h, w] because w => x, h = y
+        
+        # create Keypoints for label
+        label_tvtensor = torchvision.tv_tensors.KeyPoints(
+            original_coords,
+            canvas_size=(h, w),
+            dtype=torch.float32
+        )
+        
+        # FINALLY!
         if self.image_transform:
-            image_tensor = self.image_transform(image)
-        else:
-            image_tensor = torch.from_numpy(image)
-            # Converts from (h, w, c) into (c, h, w), which pytorch expects.
-            # https://docs.pytorch.org/vision/stable/transforms.html
-            image_tensor = image_tensor.permute(2, 0, 1)
+            img_tv, label_tv = self.image_transform(image_tvtensor, label_tvtensor)
+        
+        # NOW normalization can happen!
+        # img_tv.shape = (C, H, W)
+        new_h, new_w = img_tv.shape[-2:]
+        
+        new_coords_norm = label_tv / torch.tensor([new_w, new_h], dtype=torch.float32) # w = x, h = y
+        
+        new_coords_flat = new_coords_norm.flatten()
+        
+        # transforms might have brought some corners outside the original size
+        final_label = torch.clamp(new_coords_flat, 0.0, 0.1)
 
-        if self.label_transform:
-            label_tensor = self.label_transform(label)
-        else:
-            label_tensor = torch.from_numpy(label)
 
-        return image_tensor, label_tensor
+        return img_tv, final_label
 
     def _get_or_init_env(self):
         # The worker is FORKED by pytorch and if env is open at the time of the fork
@@ -156,19 +166,13 @@ def current_train_transforms(input_path: str, output_path: str):
     # Reverting normalization
     mean = torch.tensor(DEFAULT_WEIGHTS.transforms().mean).view(3, 1, 1) # this shape can be {operator} element-wise with (3, 1, 1) (R, G, B have different means, so we need 3 numbers in the first dimension!)
     std = torch.tensor(DEFAULT_WEIGHTS.transforms().std).view(3, 1, 1)
-    
     denormalized = img_tensor * std + mean
-    
     denormalized = denormalized.clip(0, 1) # data augmentation might have pushed values above 1 or below 0
     
     # Pytorch speaks (C, H, W), we want (H, W, C)
     result_np = denormalized.permute(1, 2, 0).numpy()
-    
     result_uint8 = (result_np * 255).astype(np.uint8) # retore values 0-255
-    
-    
     result_bgr = cv2.cvtColor(result_uint8, cv2.COLOR_RGB2BGR)
-    
     
     cv2.imwrite(output_path, result_bgr)
 
